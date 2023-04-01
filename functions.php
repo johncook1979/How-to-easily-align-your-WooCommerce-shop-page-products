44bafd0fc6a058147632b79a5483b3dda4a511b4
John Cook <41190295+johncook1979@users.noreply.github.com>
2023-04-02 09:42:17 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,6 +7,12 @@ function my_square_image_align(){
       // Square product images
       $('li.product img').height($('li.product img').width());
       
+      // To make a horizontal rectangle apply maths as below which will set the height to 75% of the width of the image (a 100px wide image will be 75px high)
+      // $('li.product img').height(($('li.product img').width())*.75);
+      
+      // To make a verticle rectangle apply maths as below which will set the height to 150% of the width of the image (50% taller than it is wide eg. 100px wide and 150px high)
+      // $('li.product img').height(($('li.product img').width())*1.5);
+      
     })( jQuery );
   </script>
 <?php
